a02761ff7f72c3f3813fecb43964133cc089d135
Daniel Brandao <danielbrandao09@hotmail.com>
2024-01-08 09:52:02 -0300

    Error: EAFP - Easy to Ask Forgiveness Than Permission

--- erros.py
+++ erros.py
@@ -3,17 +3,34 @@
 import os
 import sys
 
-if os.path.exists("names.txt"): #verifica se o arquivo existe (True/False)
-    print("O arquivo existe")
-    input("...")
-    names = open("names.txt").readlines() # Estamos lendo um arquivo sem saber o que ele contem
-else:
-    print("[Error]: file names.txt not found.")
-    sys.exit(1)
-# LBYL - look before you leap ( Olhe antes de pular/continuar)
+# EAFP - Easy to Ask Forgiveness than permission
+# (É mais fácil pedir perdão do que permissão)
 
-if len(names) >= 3:
-    print(names[2])
+try:
+    raise RuntimeError("Ocorreu um error") # usamos o Raise para forçar um error
+except Exception as e:
+    print(str(e))
+
+try: #verifica se o arquivo existe (True/False)
+    names = open("names.txt").readlines() # tenta abrir o arquivo, se não existir vai para a except # FileNotFoundError
+    1 / 1 #ZeroDivisionError
+    print(names.append) # AttributeError
+except FileNotFoundError as e: # Bare except
+    print(f"{str(e)}")
+    sys.exit(1)
+except ZeroDivisionError:
+    print("[Error]: You cant divide by zero")
+    sys.exit(1)
+except AttributeError:
+    print("[Error]: List doesn't have banana")
+    # TODO: Usar retry
 else:
+    print("Sucesso!!!") # Se o Try funcionar, ele é executado
+finally:
+    print("Execute isso sempre!!!") # Sempre é executado, mesmo com o try dando error
+
+try:
+    print(names[2]) # imprime o dado de index 2, se não existir vai para o except
+except:
     print("[Eroor]:Missing name in the list")
     sys.exit(1)
