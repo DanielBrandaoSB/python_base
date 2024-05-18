22df7dd6f7a1e2f63c2790fa69af12da16b78baa
Daniel Brandão <104859473+DanielBrandaoSB@users.noreply.github.com>
2024-05-18 10:28:19 -0300

    Update app_cap03.py

--- app_cap03.py
+++ app_cap03.py
@@ -4,7 +4,6 @@ import time
 import numpy as np
 import pandas as pd
 import streamlit as st
-import sklearn
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
