dd12030bdc1a6f3336b538bdd62c9e3bac947e85
Daniel Brandão <104859473+DanielBrandaoSB@users.noreply.github.com>
2024-05-18 10:07:32 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -4,13 +4,7 @@ import time
 import numpy as np
 import pandas as pd
 import streamlit as st
-import sklearn.metrics
-import sklearn.datasets
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import confusion_matrix
-import pydeck
+
 
 ### Teste
 st.title("Teste de Componentes")
@@ -33,9 +27,9 @@ st.title("Métricas Principais")
 col1, col2, col3 = st.columns(3)
 var = 1000
 delta = 100
-col1.metric("Volumetria Média (12 Meses)",f'{var}',f'{delta} pedidos', help=' O indicador leva em consideração o CBK de Jan/23 até Jan/24')
-col2.metric("%ChargeBack Médio (12 Meses)", '0.5%', -0.1)
-col3.metric("%Bloqueio Médio (12 Meses)", '0.3%', "0.1")
+col1.metric("Volumetria Média (12 Meses)",f'{var}',f'{delta} pedidos', help=' O indicador leva em consideração de Jan/23 até Jan/24')
+col2.metric("%CMédio (12 Meses)", '0.5%', -0.1)
+col3.metric("%B Médio (12 Meses)", '0.3%', "0.1")
 print(data_inicial)
 print(data_final)
 
